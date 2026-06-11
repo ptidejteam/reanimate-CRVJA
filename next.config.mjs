e09c839460e35c9c5af5116e701711bee65e9308
agadfs <henriquedefreitasserra@hotmail.com>
2026-06-11 12:32:21 -0400

    Removed minification

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode:false};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: false,
+  
+    webpack: (config) => {
+      config.optimization.minimize = false;
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
